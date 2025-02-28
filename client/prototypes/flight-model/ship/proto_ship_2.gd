dbc5588de5ddfb3054fa52e2a5311de367dd53b0
Justin <wilson.s.justin@gmail.com>
2025-02-27 20:52:38 -0700

    adds ship rotation, thrust in ship's direction, and adds a second ship using the RigidBody2d physics object

--- client/prototypes/flight-model/ship/proto_ship_2.gd
+++ client/prototypes/flight-model/ship/proto_ship_2.gd
@@ -1,6 +1,5 @@
-extends CharacterBody2D
+extends RigidBody2D
 
-const SPEED = 300.0
 @export var THRUST_FORCE: float = 5000.0
 @export var ROTATION_SPEED: float = 5.0
 
@@ -8,15 +7,13 @@ var forward_thrust: Vector2 = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("move_up"):
+		# velocity += Vector2(0, -THRUST_FORCE * delta)  # Apply thrust
 		forward_thrust = Vector2(0, -THRUST_FORCE * delta) # Apply thrust in the forward direction
-		velocity += forward_thrust.rotated(rotation) * delta # Rotates the thruster to point in the direction of the ship
+		linear_velocity += forward_thrust.rotated(rotation) * delta # Rotates the thruster to point in the direction of the ship
 
 	if Input.is_action_pressed("move_left"):
 		rotation -= ROTATION_SPEED * delta  # Rotate left
 	if Input.is_action_pressed("move_right"):
 		rotation += ROTATION_SPEED * delta  # Rotate right
-
-	move_and_slide()
-
 
 

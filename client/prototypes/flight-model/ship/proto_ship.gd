8407d822664e70f999e9db8d03565b58532698d7
Justin <wilson.s.justin@gmail.com>
2025-02-27 19:29:37 -0700

    added ship thrust

--- client/prototypes/flight-model/ship/proto_ship.gd
+++ client/prototypes/flight-model/ship/proto_ship.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
+@export var THRUST_FORCE: float = 200.0
 
 func _physics_process(delta: float) -> void:
-	# STUB: Put ship movement here
-	pass
-	# move_and_slide()
+	if Input.is_action_pressed("move_up"):
+		velocity += Vector2(0, -THRUST_FORCE * delta)  # Apply thrust
+		
+	move_and_slide()
+
+
+

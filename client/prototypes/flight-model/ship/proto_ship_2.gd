c840879db4a2d426de17114a9b278b06199231e5
Justin <wilson.s.justin@gmail.com>
2025-02-28 18:37:03 -0700

    changed default script to ProtoFlightModel.tscn, turned off gravity (stopped rigid ship from falling), and fixed rotation by switching to apply_torque

--- client/prototypes/flight-model/ship/proto_ship_2.gd
+++ client/prototypes/flight-model/ship/proto_ship_2.gd
@@ -2,18 +2,18 @@ extends RigidBody2D
 
 @export var THRUST_FORCE: float = 5000.0
 @export var ROTATION_SPEED: float = 5.0
+@export var TORQUE_FORCE: float = 500.0 # Rotational force
 
 var forward_thrust: Vector2 = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("move_up"):
-		# velocity += Vector2(0, -THRUST_FORCE * delta)  # Apply thrust
 		forward_thrust = Vector2(0, -THRUST_FORCE * delta) # Apply thrust in the forward direction
 		linear_velocity += forward_thrust.rotated(rotation) * delta # Rotates the thruster to point in the direction of the ship
 
 	if Input.is_action_pressed("move_left"):
-		rotation -= ROTATION_SPEED * delta  # Rotate left
+		apply_torque(-TORQUE_FORCE * delta)  # Rotate left
 	if Input.is_action_pressed("move_right"):
-		rotation += ROTATION_SPEED * delta  # Rotate right
+		apply_torque(TORQUE_FORCE * delta)  # Rotate right
 
 

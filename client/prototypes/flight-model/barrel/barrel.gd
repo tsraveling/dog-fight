3c11ec767dac1adc4f59acd135b6ac854ec9dfed
Tim Raveling <tsraveling@gmail.com>
2025-03-06 14:02:31 -0700

    Adds ship cam, reduces damping, expands barrel settings

--- client/prototypes/flight-model/barrel/barrel.gd
+++ client/prototypes/flight-model/barrel/barrel.gd
@@ -1,4 +1,7 @@
 extends RigidBody2D
 
 func _ready() -> void:
-	return
+	linear_damp_mode = RigidBody2D.DAMP_MODE_REPLACE
+	linear_damp = 0.0
+	angular_damp_mode = RigidBody2D.DAMP_MODE_REPLACE
+	angular_damp = 0.0

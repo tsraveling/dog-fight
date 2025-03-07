a3a7ced81a7aeffa75feb6b532771ef6aef89100
Tim Raveling <tsraveling@gmail.com>
2025-03-07 09:38:50 -0700

    Barrel collisions and explosions working

--- client/prototypes/flight-model/barrel/barrel.gd
+++ client/prototypes/flight-model/barrel/barrel.gd
@@ -11,18 +11,16 @@ func _ready() -> void:
 	angular_damp = 0.0
 
 	anim.animation_finished.connect(_on_animation_finished) # Attach signal for end of animation
-	connect("body_entered", _on_body_entered)  # Attach signal for collision detection
+	# connect("body_entered", _on_body_entered)  # Attach signal for collision detection
 
 func _on_body_entered(body):
-	print("Instance:", self, " collided with:", body.name)
-	trigger_explosion() # On collision, trigger the explosion
+	if body.name == "ProtoShip":
+		trigger_explosion() # On collision, trigger the explosion
 
 func trigger_explosion():
 	anim.visible = true # Keeps the animation hidden until triggered
 	anim.play("explosion_animation")
-
-	if barrel:
-		barrel.queue_free() # Hide the barrel sprite
+	barrel.visible = false
 
 func _on_animation_finished():
 	queue_free() # Free the instance after the animation is finished

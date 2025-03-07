d3baa6a3262a976ef6cb4e1b20733531ffc011e4
Tim Raveling <tsraveling@gmail.com>
2025-03-06 17:00:17 -0700

    Tim/prototype improvements (#13)
    * Removed CharBody ship
    
    * Adds ship cam, reduces damping, expands barrel settings

--- client/prototypes/flight-model/barrel/barrel_manager.gd
+++ client/prototypes/flight-model/barrel/barrel_manager.gd
@@ -2,11 +2,12 @@ extends Node2D
 
 @export var barrel_scene: PackedScene
 @export var spawn_count: int = 10
-
-# Stores the width (screen_size.x) and height (screen_size.y) of the game window
-var spawn_area: Vector2 = Vector2(1920, 1080)
+@export var spawn_diameter: float = 2000
 
 func _ready() -> void:
+	
+	var spawn_area = Vector2(spawn_diameter, spawn_diameter)
+
 	if not barrel_scene:
 		print("Assign barrel.tscn to the Barrel Scene in the Inspector.")
 		return
@@ -24,9 +25,6 @@ func _ready() -> void:
 		)
 
 		instance.position = global_position + random_position # Set the position of the instance
-		instance.gravity_scale = 0 # Turns off gravity
-		instance.linear_damp = 0 # Not working yet
-		instance.angular_damp = 0 # Not working yet
 		instance.rotation_degrees = random_rotation # Random starting rotation
 		instance.apply_impulse(random_impulse, Vector2.ZERO) # Applies the impulse to the instance
 		add_child(instance)
---
+++ client/prototypes/flight-model/ship/ProtoShip.gd
@@ -0,0 +1,33 @@
+extends RigidBody2D
+
+@export var thrust_force: float = 5000.0
+@export var rotation_speed: float = 5.0
+@export var torque_force: float = 500.0 # Rotational force
+
+@onready var thrust_polygon := $ThrustPolygon
+
+var forward_thrust: Vector2 = Vector2.ZERO
+
+func _ready() -> void:
+	linear_damp_mode = RigidBody2D.DAMP_MODE_REPLACE
+	linear_damp = 0.0
+	angular_damp_mode = RigidBody2D.DAMP_MODE_REPLACE
+	angular_damp = 0.0
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("move_up"):
+		thrust_polygon.visible = true
+		forward_thrust = Vector2(0, -thrust_force * delta) # Apply thrust in the forward direction
+		linear_velocity += forward_thrust.rotated(rotation) * delta # Rotates the thruster to point in the direction of the ship
+	else:
+		thrust_polygon.visible = false
+
+	if Input.is_action_pressed("angular_brake"):
+		angular_damp = 4
+	else:
+		angular_damp = 0.0
+
+	if Input.is_action_pressed("move_left"):
+		apply_torque(-torque_force * delta)  # Rotate left
+	if Input.is_action_pressed("move_right"):
+		apply_torque(torque_force * delta)  # Rotate right

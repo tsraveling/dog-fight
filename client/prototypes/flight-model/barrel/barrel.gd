c4a01b53a889f3a267ec292da50162458d928a3c
Justin Wilson <wilson.s.justin@gmail.com>
2025-03-07 09:41:48 -0700

    WIP: add barrel explosion animation (#14)
    * WIP: add barrel explosion animation
    
    * Barrel collisions and explosions working
    
    ---------
    
    Co-authored-by: Tim Raveling <tsraveling@gmail.com>

--- client/prototypes/flight-model/barrel/barrel.gd
+++ client/prototypes/flight-model/barrel/barrel.gd
@@ -1,7 +1,26 @@
+
 extends RigidBody2D
 
+@onready var anim = $ExplosionAnimation
+@onready var barrel = $Sprite2D
+
 func _ready() -> void:
-	linear_damp_mode = RigidBody2D.DAMP_MODE_REPLACE
+	linear_damp_mode = RigidBody2D.DAMP_MODE_REPLACE # Not working when located in parent
 	linear_damp = 0.0
 	angular_damp_mode = RigidBody2D.DAMP_MODE_REPLACE
 	angular_damp = 0.0
+
+	anim.animation_finished.connect(_on_animation_finished) # Attach signal for end of animation
+	# connect("body_entered", _on_body_entered)  # Attach signal for collision detection
+
+func _on_body_entered(body):
+	if body.name == "ProtoShip":
+		trigger_explosion() # On collision, trigger the explosion
+
+func trigger_explosion():
+	anim.visible = true # Keeps the animation hidden until triggered
+	anim.play("explosion_animation")
+	barrel.visible = false
+
+func _on_animation_finished():
+	queue_free() # Free the instance after the animation is finished
